bba751246a48629ec76680300bd273b08b86dd2c
Mikkel Malmberg <mikkel@brnbw.com>
2025-09-26 18:51:29 +0200

    Migrate to Prettier (#1)
    * Migrate to Prettier
    
    * Apply Prettier

--- test/index.test.js
+++ test/index.test.js
@@ -6,7 +6,7 @@ const IPC = require('pear-ipc')
 const { isWindows } = require('which-runtime')
 const message = require('..')
 
-function pipeId (s) {
+function pipeId(s) {
   const buf = b4a.allocUnsafe(32)
   sodium.crypto_generichash(buf, b4a.from(s))
   return b4a.toString(buf, 'hex')
@@ -15,11 +15,13 @@ function pipeId (s) {
 test('message', async (t) => {
   t.plan(1)
   const kIPC = Symbol('test.ipc')
-  const socketPath = isWindows ? `\\\\.\\pipe\\test-${pipeId(__dirname)}` : __dirname + '/test.sock' // eslint-disable-line
+  const socketPath = isWindows
+    ? `\\\\.\\pipe\\test-${pipeId(__dirname)}`
+    : __dirname + '/test.sock' // eslint-disable-line
   const srv = new IPC.Server({
     socketPath,
     handlers: {
-      message (msg) {
+      message(msg) {
         t.alike(msg, { some: 'props' })
       }
     }
@@ -31,7 +33,9 @@ test('message', async (t) => {
   await ipc.ready()
   class API {
     static IPC = kIPC
-    get [kIPC] () { return ipc }
+    get [kIPC]() {
+      return ipc
+    }
   }
   global.Pear = new API()
 
